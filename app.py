0e6c41b22b2efea42dba69b4269053a67ffd0ec9
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-11 16:02:50 +0700

    version 0.1.9

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ db = mongo.db
 
 
 if __name__ == '__main__':
-    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+
 
     from routes.ask_bp import ask_bp
     app.register_blueprint(ask_bp)
     from routes.faqs_bp import faqs_bp
     app.register_blueprint(faqs_bp)
 
-    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
+    app.run(host='0.0.0.0', port=5000)
