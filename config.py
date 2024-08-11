e51a4ba599b701ff27c217a1fd700e35ee9555f8
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-11 19:46:43 +0700

    version 0.2.7

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r'/*': {'origins': '*'}})
-    app.config["MONGO_URI"] = os.getenv("MONGO_IP_ADDRESS")
+    app.config["MONGO_URI"] = "mongodb://67.205.191.74:27017/ascend-edu"
 
     from utils.extensions import timeloop
     timeloop.init_app(app)
