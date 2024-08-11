318179cbc3f43cbd7a423ca7f79df374d72200f3
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-11 22:29:10 +0700

    testttttt

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r'/*': {'origins': '*'}})
-    app.config["MONGO_URI"] = os.environ['MONGO_URI']
+    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
     from utils.extensions import timeloop
     timeloop.init_app(app)
     timeloop.start()
