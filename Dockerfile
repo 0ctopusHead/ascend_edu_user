89ead987ca5f070a1f19108cf36b2481ac4a93aa
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-11 15:43:18 +0700

    version 0.1.6

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,18 @@
 FROM python:3.10
-WORKDIR /app
-COPY . /app
+
+ENV GROUP_ID=1000 \
+    USER_ID=1000
+
+WORKDIR /var/www
+COPY . /var/www
+
+
 RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 80
-CMD ["gunicorn", "--bind", "0.0.0.0:80", "app:create_app()"]
+RUN pip install gunicorn
+
+RUN addgroup -g $GROUP_ID www
+RUN adduser -D -u $USER_ID -G www www -s /bin/sh
+
+USER www
+EXPOSE 5000
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:create_app()"]
--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask_pymongo import PyMongo
 from config import create_app
-
+import os
 app = create_app()
 mongo = PyMongo(app)
 db = mongo.db
 
 
 if __name__ == '__main__':
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+
     from routes.ask_bp import ask_bp
     app.register_blueprint(ask_bp)
     from routes.faqs_bp import faqs_bp
     app.register_blueprint(faqs_bp)
 
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
