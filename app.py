80bd96ce1018cdaa2fe24d625dc55aef669c338f
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-11 19:31:45 +0700

    version 0.2.6

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask_pymongo import PyMongo
 from config import create_app
 import os
+
 app = create_app()
 mongo = PyMongo(app)
 db = mongo.db
 
 
-if __name__ == '__main__':
-
+def register_blueprints(app):
     from routes.ask_bp import ask_bp
     app.register_blueprint(ask_bp)
+
     from routes.faqs_bp import faqs_bp
     app.register_blueprint(faqs_bp)
 
-    @app.route("/")
-    def favicon():
-        return "Heeeeeeee", 200
 
+register_blueprints(app)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app, resources={r'/*': {'origins': '*'}})
     app.config["MONGO_URI"] = os.getenv("MONGO_IP_ADDRESS")
+
     from utils.extensions import timeloop
     timeloop.init_app(app)
     timeloop.start()
