dd02b11badb784b32dea384993c7acb79705075d
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-12 20:09:14 +0700

    dfaszdfsdffsd

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 import os
-from dotenv import load_dotenv
 
 
 def create_app():
-    load_dotenv()
     app = Flask(__name__)
     CORS(app, resources={r'/*': {'origins': '*'}})
     MONGO_URI = os.environ.get('MONGO_URI')
