bc65462d0e298fd654f1ed0699b6e123eb530c75
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-13 19:51:39 +0700

    version 0.4.5

--- routes/ask_bp.py
+++ routes/ask_bp.py
@@ -6,7 +6,7 @@ from linebot.v3.exceptions import (
 )
 from linebot.models import (MessageEvent,
                             TextMessage,
-                            TextSendMessage, ButtonsTemplate, TemplateSendMessage,
+                            TextSendMessage, CarouselTemplate, CarouselColumn, TemplateSendMessage,
                             MessageAction)
 from app import app, handler, line_bot_api
 
@@ -39,17 +39,20 @@ def handle_message(event):
         if query == "faqs":
             faqs = faqs_controller.get_faqs()
             if faqs:
-                buttons_template = ButtonsTemplate(
-                    title='FAQs',
-                    text='Please select a FAQ:',
-                    actions=[
-                        MessageAction(label=faq["question"], text=faq["question"])
-                        for faq in faqs
-                    ]
-                )
+                faqs_list = faqs[:5]
+                columns = [
+                    CarouselColumn(
+                        title='FAQ',
+                        text=faq["question"],
+                        actions=[MessageAction(label='See Answer', text=faq["question"])]  # Use a short label here
+                    )
+                    for faq in faqs_list
+                ]
+
+                carousel_template = CarouselTemplate(columns=columns)
                 template_message = TemplateSendMessage(
-                    alt_text='FAQs',
-                    template=buttons_template
+                    alt_text='FAQs Carousel',
+                    template=carousel_template
                 )
             else:
                 template_message = TextSendMessage(text="No FAQs are available at the moment.")
@@ -60,10 +63,11 @@ def handle_message(event):
             response_message = ask_controller.ask_endpoint(query)
             faqs_controller.handle_user_query(query)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response_message))
+
     except Exception as e:
         app.logger.error(f"Exception in handle_message: {e}")
 
-
+        
 @ask_bp.route('/ask/', methods=['POST'])
 def ask_endpoint():
     data = request.get_json()
