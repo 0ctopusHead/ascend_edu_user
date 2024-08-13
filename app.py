803d2eacbe0a1faf313979ab06ab7060a1d6ba2c
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-08-13 15:07:47 +0700

    version 0.3.8

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask_pymongo import PyMongo
-
 from config import create_app
+from linebot import LineBotApi, WebhookHandler
 import os
 
 app = create_app()
 
 mongo = PyMongo(app)
 db = mongo.db
+
+chanel_secret = os.environ.get('CHANEL_SECRET')
+chanel_access_token = os.environ.get('CHANEL_ACCESS_TOKEN')
+
+line_bot_api = LineBotApi(chanel_access_token)
+handler = WebhookHandler(chanel_secret)
 
 
 def register_blueprints(app):
--- routes/ask_bp.py
+++ routes/ask_bp.py
@@ -1,21 +1,42 @@
 from flask import jsonify, Blueprint, request
 from controller.AskController import AskController
 from controller.FAQsController import FAQsController
-
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (MessageEvent,
+                            TextMessage,
+                            TextSendMessage)
+from app import app, handler, line_bot_api
 ask_bp = Blueprint('ask_bp', __name__)
 
 
+@ask_bp.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        raise
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
 @ask_bp.route('/ask/', methods=['POST'])
-def ask_endpoint():
-    data = request.get_json()
-    query = data.get('query')
+def ask_endpoint(event):
+    query = event.message.text
     ask_controller = AskController()
     faqs_controller = FAQsController()
     response_message = ask_controller.ask_endpoint(query)
     faqs_controller.handle_user_query(query)
+    line_bot_api.reply_message(event.reply_token,
+                               TextSendMessage(response_message))
 
     return jsonify(
-
         {'response_message': response_message})
 
 
